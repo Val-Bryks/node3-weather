eadf3348cd6f258817dd0863ccdf6951afe5a858
Valerie <valeriebryksina@gmail.com>
2021-01-17 19:40:48 +0200

    Add humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location!', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ". It is current " + body.current.temperature + ' degress out. It is humidity ' + body.current.humidity + ' degress out.')
+            callback(undefined, body.current.weather_descriptions[0] + ". It is current " + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degress out. The humidity is ' + body.current.humidity + ' %.')
         }
     })
 
